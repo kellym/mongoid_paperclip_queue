21339112aab913ad92adde4d61df4888c46f1c8b
re5et <re5etsmyth@gmail.com>
2012-05-04 14:50:20 -0700

    hack to allow use of non-queued attachments

--- lib/mongoid_paperclip_queue.rb
+++ lib/mongoid_paperclip_queue.rb
@@ -1,4 +1,3 @@
-
 begin
   require "paperclip"
 rescue LoadError
@@ -7,86 +6,95 @@ rescue LoadError
 end
 
 module Mongoid::PaperclipQueue
-  
-    class Queue
-      
-      @queue = :paperclip
-      
-      def self.enqueue(klass,field,id,*parents)
-        ::Resque.enqueue(self,klass,field,id,*parents)
-      end
-      def self.perform(klass,field,id,*parents)
-        if parents.empty?
-          klass = klass.constantize
-        else
-          p = parents.shift
-          parent = p[0].constantize.find(p[2])
-          parents.each do |p|
-            parent = parent.send(p[1].to_sym).find(p[2])
-          end
-          klass = parent.send(klass.to_sym)
-        end
-        klass.find(id).do_reprocessing_on field
-      end
-       
+
+  class Queue
+
+    @queue = :paperclip
+
+    def self.enqueue(klass,field,id,*parents)
+      ::Resque.enqueue(self,klass,field,id,*parents)
     end
-    
-    module Redis
-      def server=(srv)
-        case srv
-        when String
-          if srv =~ /redis\:\/\//
-            server = ::Redis.connect(:url => srv, :thread_safe => true)
-          else
-            srv, namespace = srv.split('/', 2)
-            host, port, db = srv.split(':')
-            server = ::Redis.new(:host => host, :port => port,
-              :thread_safe => true, :db => db)
-          end
-          namespace ||= :delayed
-      
-          @server = ::Redis::Namespace.new(namespace, :redis => redis)
-        when ::Redis::Namespace
-          @server = srv
-        else
-          @server = ::Redis::Namespace.new(:delayed, :redis => srv)
+    def self.perform(klass,field,id,*parents)
+      if parents.empty?
+        klass = klass.constantize
+      else
+        p = parents.shift
+        parent = p[0].constantize.find(p[2])
+        parents.each do |p|
+          parent = parent.send(p[1].to_sym).find(p[2])
         end
+        klass = parent.send(klass.to_sym)
       end
-      
-      def server
-        return @server if @server
-        self.server = ::Redis.respond_to?(:connect) ? ::Redis.connect : "localhost:6379"
-        self.server
-      end        
-      extend self
+      klass.find(id).do_reprocessing_on field
     end
-    
-    def has_queued_attached_file(field, options = {})
 
+  end
 
-      # Include Paperclip and Paperclip::Glue for compatibility
-      unless self.ancestors.include?(::Paperclip)
-        include ::Paperclip
-        include ::Paperclip::Glue
+  module Redis
+    def server=(srv)
+      case srv
+      when String
+        if srv =~ /redis\:\/\//
+          server = ::Redis.connect(:url => srv, :thread_safe => true)
+        else
+          srv, namespace = srv.split('/', 2)
+          host, port, db = srv.split(':')
+          server = ::Redis.new(:host => host, :port => port,
+                               :thread_safe => true, :db => db)
+        end
+        namespace ||= :delayed
+
+        @server = ::Redis::Namespace.new(namespace, :redis => redis)
+      when ::Redis::Namespace
+        @server = srv
+      else
+        @server = ::Redis::Namespace.new(:delayed, :redis => srv)
       end
-      
-      #send :include, InstanceMethods
-      include InstanceMethods
+    end
 
-      # Invoke Paperclip's #has_attached_file method and passes in the
-      # arguments specified by the user that invoked Mongoid::Paperclip#has_mongoid_attached_file
-      if options[:logger].nil? && Mongoid::Config.logger.present?
-        options[:logger] = Mongoid::Config.logger
-      end
-      has_attached_file(field, options)
-      
+    def server
+      return @server if @server
+      self.server = ::Redis.respond_to?(:connect) ? ::Redis.connect : "localhost:6379"
+      self.server
+    end
+    extend self
+  end
+
+  def has_mongoid_attached_file(field, options = {}, &block)
+    # Include Paperclip and Paperclip::Glue for compatibility
+    unless self.ancestors.include?(::Paperclip)
+      include ::Paperclip
+      include ::Paperclip::Glue
+    end
+
+    #send :include, InstanceMethods
+    include InstanceMethods
+
+    # Invoke Paperclip's #has_attached_file method and passes in the
+    # arguments specified by the user that invoked Mongoid::Paperclip#has_mongoid_attached_file
+    if options[:logger].nil? && Mongoid::Config.logger.present?
+      options[:logger] = Mongoid::Config.logger
+    end
+    has_attached_file(field, options)
+
+    yield block if block_given?
+    ##
+    # Define the necessary collection fields in Mongoid for Paperclip
+    field(:"#{field}_file_name", :type => String)
+    field(:"#{field}_content_type", :type => String)
+    field(:"#{field}_file_size", :type => Integer)
+    field(:"#{field}_updated_at", :type => DateTime)
+  end
+
+  def has_queued_mongoid_attached_file(field, options = {})
+    has_mongoid_attached_file(field, options) do
       # halt processing initially, but allow override for reprocess!
       self.send :"before_#{field}_post_process", :halt_processing
-      
-      define_method "#{field}_processing!" do 
+
+      define_method "#{field}_processing!" do
         true
       end
-      
+
       self.send :after_save do
         if self.changed.include? "#{field}_updated_at"
           # add a Redis key for the application to check if we're still processing
@@ -108,7 +116,7 @@ module Mongoid::PaperclipQueue
             end
             # we need the relation name, not the class name
             args = [ self.metadata.name, field, self.id.to_s] + parents.reverse
-          else 
+          else
             # or just use our default params like any other Paperclip model
             args = [self.class.name, field, self.id.to_s]
           end
@@ -116,35 +124,37 @@ module Mongoid::PaperclipQueue
           # then queue up our processing
           Mongoid::PaperclipQueue::Queue.enqueue(*args)
         end
-      end
-      
-      ## 
-      # Define the necessary collection fields in Mongoid for Paperclip
-      field(:"#{field}_file_name", :type => String)
-      field(:"#{field}_content_type", :type => String)
-      field(:"#{field}_file_size", :type => Integer)
-      field(:"#{field}_updated_at", :type => DateTime)
-    end  
 
-    module InstanceMethods
-      
-      def halt_processing
-        false if @is_processing.nil?  # || false
-      end
-            
-      def do_reprocessing_on(field)
-        @is_processing=true
-        self.send(field.to_sym).reprocess!
-        Mongoid::PaperclipQueue::Redis.server.srem(self.class.name, "#{field}:#{self.id.to_s}")
       end
 
     end
+
+  end
+
+  module InstanceMethods
+
+    def halt_processing
+      false if @is_processing.nil?  # || false
+    end
+
+    def do_reprocessing_on(field)
+      @is_processing=true
+      self.send(field.to_sym).reprocess!
+      Mongoid::PaperclipQueue::Redis.server.srem(self.class.name, "#{field}:#{self.id.to_s}")
+    end
+
+  end
+
 end
+
 module Paperclip
+
   class Attachment
+
     def processing?
       @instance.respond_to?(:"#{name}_processing!") && (@instance.new_record? || Mongoid::PaperclipQueue::Redis.server.sismember(@instance.class.name, "#{@name}:#{@instance.id.to_s}"))
-    end    
-    
+    end
+
   end
+
 end
